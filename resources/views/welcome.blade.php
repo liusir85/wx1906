63296a6a28f37690a7b1dfd044e5df3a5f85a93c
liusir85 <893518673@qq.com>
2020-02-07 08:32:37 +0000

    shuru

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    wx1906
                 </div>
 
                 <div class="links">
