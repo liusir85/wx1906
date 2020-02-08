03df06fcdad9fd0717331fd7bdd29619181417e6
liusir85 <893518673@qq.com>
2020-02-08 03:49:55 +0000

    123123

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    wx1906
+                    123123
                 </div>
 
                 <div class="links">
